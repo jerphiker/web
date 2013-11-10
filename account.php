f03f2d4676e9fb6630c36e92a3983e4ef952eec4
jerphiker <jkerr@mines.edu>
2013-11-10 00:48:16 -0700

    Added details for each page that need to be completed before the page will be done

--- account.php
+++ account.php
@@ -11,9 +11,19 @@
 	include './includes/header.php';
 
 	include './includes/menu.php';
+?>
 
-	echo "Body goes here<br />\r\n";
+<p>
+This page needs to display username, password, and recovery email (*possibly age and gender, if that information is collected for accounts, so people with accounts don't need to enter it every time.*)
 
+It should offer forms to change password, recovery email, and username. If the new recovery email or username already exists in the users table, then an error should be displayed and the user should be returned to the form they were filling out.
+
+If someone tries to go to the account page without being logged in, it should renavigate them to the login page (index.php)
+
+An option should be displayed to delete the user account, with a checkbox to also delete all their prior status updates associated with that account. If not, then the updates should be altered so they are no longer associated with an account (so if someone later creates an account by the same username, they do not have access to someone else's status updates).
+</p>
+
+<?php
 	include './includes/footer.php';
 ?>
 </body>
--- region.php
+++ region.php
@@ -11,9 +11,15 @@
 	include './includes/header.php';
 
 	include './includes/menu.php';
+?>
 
-	echo "Body goes here<br />\r\n";
+<p>
+This page should have a form on the left side, and a map on the right with all relevant data points for a region.
+The form should have fields that can be used to narrow down the results shown on the map (based on information collected with
+the status reports). This will allow users to choose a demographic more closely related to their own.
+</p>
 
+<?php
 	include './includes/footer.php';
 ?>
 </body>
